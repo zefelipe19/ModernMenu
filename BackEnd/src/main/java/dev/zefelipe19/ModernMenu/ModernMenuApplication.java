3be6aca8fcf1e29e6d440f28cf8751c0ee9b8d4b
Felipe <felipe.paula1903@gmail.com>
2025-04-04 12:36:49 -0300

    Instalação inicial do Spring no BackEnd e do Angular no FrontEnd

---
+++ BackEnd/src/main/java/dev/zefelipe19/ModernMenu/ModernMenuApplication.java
@@ -0,0 +1,13 @@
+package dev.zefelipe19.ModernMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernMenuApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/dev/zefelipe19/ModernMenu/ModernMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zefelipe19.ModernMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
